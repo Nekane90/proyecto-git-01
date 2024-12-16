b09c79b3b6d74c857320369c5b18bf56e13edc87
nekane90 <mixhardcore@hotmail.com>
2024-12-16 08:24:13 +0100

    Commit inicial

---
+++ src/paquetegit/Empleado.java
@@ -0,0 +1,13 @@
+package paquetegit;
+
+public class Empleado {
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private byte edad;
+	private float sueldo;
+	private boolean hijos;
+	
+	
+	
+}
