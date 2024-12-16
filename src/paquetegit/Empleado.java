b1202893e053094d8a1203d0c3f437d70257afa4
nekane90 <mixhardcore@hotmail.com>
2024-12-16 08:52:13 +0100

    añadido constructor completo

--- src/paquetegit/Empleado.java
+++ src/paquetegit/Empleado.java
@@ -1,6 +1,7 @@
 package paquetegit;
 
 public class Empleado {
+
 	private String dni;
 	private String nombre;
 	private String apellidos;
@@ -8,6 +9,16 @@ public class Empleado {
 	private float sueldo;
 	private boolean hijos;
 	
-	
+	/////Constructores////
+	public Empleado(String dni, String nombre, String apellidos, byte edad, float sueldo, boolean hijos) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+		this.sueldo = sueldo;
+		this.hijos = hijos;
+	}
 	
 }
+
